8608abf273da9eb849bce7b753ae5026fd6caa52
Ratnesh121333 <yratneshyadav166@gmail.com>
2025-05-11 10:22:40 +0530

    Add files via upload

---
+++ bookingform.js
@@ -0,0 +1,20 @@
+// const form = document.getElementById('shipment-form');
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   const fullName = document.getElementById('full-name').value.trim();
+//   const email = document.getElementById('email').value.trim();
+//   const phone = document.getElementById('phone').value.trim();
+//   const pickupLocation = document.getElementById('pickup-location').value.trim();
+//   const dropOffLocation = document.getElementById('drop-off-location').value.trim();
+//   const pickupDate = document.getElementById('pickup-date').value.trim();
+//   const shipmentType = document.getElementById('shipment-type').value.trim();
+//   const paymentMethod = document.getElementById('payment-method').value.trim();
+
+//   if (fullName === '' || email === '' || phone === '' || pickupLocation === '' || dropOffLocation === '' || pickupDate === '' || shipmentType === '' || paymentMethod === '') {
+//     alert('Please fill in all required fields');
+//   } else {
+//     form.submit();
+//   }
+// });
